6c662202627681f7d8346aef0d989209788a5e82
JakeSmith1 <jakesmith5757@gmail.com>
2016-12-07 13:08:23 -0500

    features(binary search, bubble sort, insertion sort, mergeSort)

---
+++ bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(array) {
+  var swapped;
+  var firstRun = true;
+
+  while(firstRun || swapped) {
+    firstRun = false;
+    swapped = false;
+
+    for(let i = 0; i < array.length; i += 1) {
+      if(array[i] > array[i+1]) {
+        let temp = array[i];
+        array[i] = array[i+1];
+        array[i+1] = temp;
+        swapped = true;
+      }
+    }
+  }
+  return array;
+}
