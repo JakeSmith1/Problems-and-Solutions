40f50613192f606397b65a3c7fdcda39161d0176
JakeSmith1 <jakesmith5757@gmail.com>
2016-12-09 07:35:01 -0500

    fix(linked List), feature(merge sort tests, linked list tests)

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # Problems-and-Solutions
 This repository contains problems I have done in my spare time.
 
-##Sorting Algorithms: 
+##To Run Tests:
+- open index.html in your browser
+
+##Sorting Algorithms:
 - Bubble sort
 - Insertion sort
 - Merge sort - recursive and iterative
 
+##Strings and Array Problems:
+-
+
+##Linked Lists:
--- tests/sort_tests.js
+++ tests/sort_tests.js
@@ -1,19 +1,73 @@
 var assert = chai.assert;
 var expect = chai.expect;
+var should = chai.should();
 
 //describe groups tests
 describe('Bubble Sort', function() {
-  var input = [3,2,1];
   //it describes individual tests
   it('should be a function', function() {
     expect(bubbleSort).to.be.a('function');
   })
   it('should return an array', function() {
+    let input = [8,0,6,2,1];
     let output = bubbleSort(input);
     expect(output).to.be.an('array');
   });
   it('should return a sorted array', function() {
+    let input = [8,0,3,6,2,1];
     let output = bubbleSort(input);
-    expect(output).to.eql([1,2,3]);
+    expect(output.toString()).to.eql([0,1,2,3,6,8].toString());
+  });
+  it('should handle negative numbers and repeats', function() {
+    let input = [20,-1,4,18,-44];
+    var expected = [-44,-1,4,18,20].toString();
+    var actual = bubbleSort(input).toString();
+    actual.should.equal(expected);
+  });
+});
+
+describe('Insertion Sort', function() {
+  //it describes individual tests
+  it('should be a function', function() {
+    expect(insertionSort).to.be.a('function');
+  })
+  it('should return an array', function() {
+    let input = [8,0,6,2,1];
+    let output = insertionSort(input);
+    expect(output).to.be.an('array');
+  });
+  it('should return a sorted array', function() {
+    let input = [8,0,3,6,2,1];
+    let output = insertionSort(input);
+    expect(output.toString()).to.eql([0,1,2,3,6,8].toString());
+  });
+  it('should handle negative numbers and repeats', function() {
+    let input = [20,-1,4,18,-44];
+    var expected = [-44,-1,4,18,20].toString();
+    var actual = insertionSort(input).toString();
+    actual.should.equal(expected);
+  });
+});
+
+describe('Merge Sort', function() {
+  //it describes individual tests
+  it('should be a function', function() {
+    expect(splitThenSort).to.be.a('function');
+  })
+  it('should return an array', function() {
+    let input = [8,0,6,2,1];
+    let output = splitThenSort(input);
+    expect(output).to.be.an('array');
+  });
+  it('should return a sorted array', function() {
+    let input = [8,0,3,6,2,1];
+    let output = splitThenSort(input);
+    expect(output.toString()).to.eql([0,1,2,3,6,8].toString());
+  });
+  it('should handle negative numbers and repeats', function() {
+    let input = [20,-1,4,18,-44];
+    var expected = [-44,-1,4,18,20].toString();
+    var actual = splitThenSort(input).toString();
+    actual.should.equal(expected);
   });
 });
